028892e855673f99f1e5cd476ac3e2dcb17c6d7b
Anthony <anthony.aubertin@gmail.com>
2015-05-22 17:50:40 +0200

    imafe slice

---
+++ app/views/slices/imageFull.php
@@ -0,0 +1,9 @@
+<div class="row-separate imageFull <?= $slice->getLabel() ?>">
+<?php 
+	$items = $slice->getValue()->getArray();
+	$item = $items[0];
+	$illustrationUrl = $item->get('illustration') ? $item->get('illustration')->getMain()->getUrl() : null;
+?>	
+  <img src="<?= $illustrationUrl ?>" />
+
+</div>
